3847202605ab003339ab6bd0cd69d947208e0553
Krish <krishramani111@gmail.com>
2026-01-14 17:18:29 +0530

    - apple watch target added

---
+++ ios/Example Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  example Watch App
+//
+//  Created by Krish Ramani on 14/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios/Example Watch App/exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  exampleApp.swift
+//  example Watch App
+//
+//  Created by Krish Ramani on 14/01/26.
+//
+
+import SwiftUI
+
+@main
+struct example_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
